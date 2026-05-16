e63c601d6bb4f4efc62fbfd75226b3e780ae30ce
elcnburak <elcnburak@hotmail.com>
2026-05-16 18:07:48 +0100

    Day01_Practice

---
+++ src/main/java/Day01/Day01_Practice.java
@@ -0,0 +1,20 @@
+package Day01;
+
+public class Day01_Practice {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Worl");
+        System.out.println("My name is Java");
+
+        // short comment
+        /* long comment */
+
+        System.out.println("Hello"); // ln means to move to next line
+        System.out.print("Hello1"); // no ln means move to same line
+
+
+    }
+
+
+}
