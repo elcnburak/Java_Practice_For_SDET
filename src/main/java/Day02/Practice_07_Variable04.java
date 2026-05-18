d12308ca051cd1bf0c30273d94c658cd1bf78e77
elcnburak <elcnburak@hotmail.com>
2026-05-18 19:06:54 +0100

    Variable

---
+++ src/main/java/Day02/Practice_07_Variable04.java
@@ -0,0 +1,10 @@
+package Day02;
+
+public class Practice_07_Variable04 {
+    public static void main(String[] args) {
+        int total;
+        int counter=0;
+        counter=counter+2;
+        System.out.println("counter = " + counter);
+    }
+}
