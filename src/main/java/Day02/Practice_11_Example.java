9b4be199b14ee8e177ee44fb9745a365df258e89
elcnburak <elcnburak@hotmail.com>
2026-05-18 19:10:24 +0100

    Example

---
+++ src/main/java/Day02/Practice_11_Example.java
@@ -0,0 +1,19 @@
+package Day02;
+
+public class Practice_11_Example {
+    public static void main(String[] args) {
+        // Question: The user has received 2 donations.
+        // Assign the donation amounts as integers to 2 variables: 500 Euro and 1000 Euro
+        // Then print the total donation amount.
+
+        int donation1=500;
+        int donation2=1000;
+        int total;
+
+        total=donation1+donation2;
+
+        System.out.println("Total = " + total+" Euro");
+        System.out.println("Total = " + (donation1+donation2)+" Euro");
+
+    }
+}
