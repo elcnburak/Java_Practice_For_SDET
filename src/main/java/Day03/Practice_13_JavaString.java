9f513723a7e18610f3566fb4dfcb40397c48acd0
elcnburak <elcnburak@hotmail.com>
2026-05-18 19:42:31 +0100

    Java String

---
+++ src/main/java/Day03/Practice_13_JavaString.java
@@ -0,0 +1,18 @@
+package Day03;
+
+public class Practice_13_JavaString {
+    public static void main(String[] args) {
+
+     String name="Elcin";
+        System.out.println("name = " + name);
+
+     String surname="Burak";
+        System.out.println("surname = " + surname);
+
+     String FullName=name+surname;
+
+        System.out.println("FullName = " + FullName);
+        System.out.println("FullName = " + name+" "+surname);
+
+    }
+}
