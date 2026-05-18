1055cb636faa9e3a47a3dc10d9a3e0d4b312371a
elcnburak <elcnburak@hotmail.com>
2026-05-18 19:09:38 +0100

    Booelan

---
+++ src/main/java/Day02/Practice_10_Boolean.java
@@ -0,0 +1,12 @@
+package Day02;
+
+public class Practice_10_Boolean {
+    public static void main(String[] args) {
+
+        //It has only 2 possible values: true or false. It is used to store this type of value.
+
+        boolean countinue=false; // true or false
+
+        System.out.println("countinue = " + countinue);
+    }
+}
