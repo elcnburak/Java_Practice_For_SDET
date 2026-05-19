0d76aed7b9c0406a53727bd258ebce4f781063bf
elcnburak <elcnburak@hotmail.com>
2026-05-19 20:03:49 +0100

    Java Scanner

---
+++ src/main/java/Day04/Practice_21_Java_Scanner.java
@@ -0,0 +1,16 @@
+package Day04;
+
+import java.util.Scanner;
+
+public class Practice_21_Java_Scanner {
+    public static void main(String[] args) {
+
+        Scanner reader=new Scanner(System.in);
+        System.out.println("Please Enter number");
+        int enterNumber=reader.nextInt(); // Wait to enter number from user
+
+        System.out.println("enterNumber = " + enterNumber);
+        System.out.println("The End");
+
+    }
+}
