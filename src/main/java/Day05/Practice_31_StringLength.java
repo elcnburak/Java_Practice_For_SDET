fe805b4fd0c041a639a0efea1009ed981b1e316a
elcnburak <elcnburak@hotmail.com>
2026-05-20 22:21:07 +0100

    Day05 String Length

---
+++ src/main/java/Day05/Practice_31_StringLength.java
@@ -0,0 +1,13 @@
+package Day05;
+
+public class Practice_31_StringLength {
+    public static void main(String[] args) {
+
+        String sentences=" The weather is amazing today";
+        int length1=sentences.length();
+
+        System.out.println("length = " + length1);
+        System.out.println("length= " + sentences.length());
+
+    }
+}
