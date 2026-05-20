4e5eff46d3bea7499a24e87f4e574e1ac8ff1bcc
elcnburak <elcnburak@hotmail.com>
2026-05-20 22:21:23 +0100

    Day05 String ChartAt

---
+++ src/main/java/Day05/Practice_32_StringCharAt.java
@@ -0,0 +1,14 @@
+package Day05;
+
+public class Practice_32_StringCharAt {
+    public static void main(String[] args) {
+
+        String sentence="Hello World";
+        System.out.println("sentence.length() = " + sentence.length());
+
+        char first=sentence.charAt(0); // 0 number bring first index
+        System.out.println("first = " + first);
+
+
+    }
+}
